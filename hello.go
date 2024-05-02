f6b772f7105e72925596a1ac8fb7872fff737816
Muhammad-Abdullah012 <not.muhammad.abdullah68@gmail.com>
2024-05-02 12:02:44 +0500

    Imported and used local module

--- hello.go
+++ hello.go
@@ -2,8 +2,10 @@ package main;
 
 import "fmt";
 import "rsc.io/quote"; // Go packages are on https://pkg.go.dev/
+import "greetings"; // 
 
 func main() {
 	fmt.Println("Hello world!"); // from import "fmt"
 	fmt.Println(quote.Glass()); // from import "rsc.io/quote" => "quote"
+	fmt.Println(greetings.Hello("Abdullah")); // from import "greetings" => "greetings"
 }
